e4dc77b08cfcfa578b637751b5abd28638372b86
guisi-esparta <guisi@esparta.io>
2019-09-29 16:40:06 -0300

    First commit

---
+++ src/main/java/br/guisi/specs/springspecs/SpringspecsApplication.java
@@ -0,0 +1,13 @@
+package br.guisi.specs.springspecs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringspecsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringspecsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/guisi/specs/springspecs/SpringspecsApplicationTests.java
@@ -0,0 +1,16 @@
+package br.guisi.specs.springspecs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringspecsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
